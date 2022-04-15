53cbb7d7ad05d7ed10e3354f7e3368ffd70bbbbb
derek-corcoran-barrios <derek.corcoran.barrios@gmail.com>
2022-04-15 19:11:50 +0200

    Initial commit

---
+++ dev/01_start.R
@@ -0,0 +1,63 @@
+# Building a Prod-Ready, Robust Shiny Application.
+#
+# README: each step of the dev files is optional, and you don't have to
+# fill every dev scripts before getting started.
+# 01_start.R should be filled at start.
+# 02_dev.R should be used to keep track of your development during the project.
+# 03_deploy.R should be used once you need to deploy your app.
+#
+#
+########################################
+#### CURRENT FILE: ON START SCRIPT #####
+########################################
+
+## Fill the DESCRIPTION ----
+## Add meta data about your application
+##
+## /!\ Note: if you want to change the name of your app during development,
+## either re-run this function, call golem::set_golem_name(), or don't forget
+## to change the name in the app_sys() function in app_config.R /!\
+##
+golem::fill_desc(
+  pkg_name = "PlotExplorer",
+  pkg_title = "Exploring Plot data in Denmark",
+  pkg_description = "Compare plots that you have sampled in Denmark with a database from the Novana Protocol.",
+  author_first_name = "Derek",
+  author_last_name = "Corcoran",
+  author_email = "derek.corcoran@bio.au.dk",
+  repo_url = NULL
+)
+
+## Set {golem} options ----
+golem::set_golem_options()
+
+## Create Common Files ----
+## See ?usethis for more information
+usethis::use_mit_license("Derek Corcoran") # You can set another license here
+usethis::use_readme_rmd(open = TRUE)
+# Note that `contact` is required since usethis version 2.1.5
+# If your {usethis} version is older, you can remove that param
+usethis::use_code_of_conduct(contact = "Derek Corcoran")
+usethis::use_lifecycle_badge("Experimental")
+usethis::use_news_md(open = FALSE)
+
+## Use git ----
+usethis::use_git()
+
+## Init Testing Infrastructure ----
+## Create a template for tests
+golem::use_recommended_tests()
+
+## Favicon ----
+# If you want to change the favicon (default is golem's one)
+golem::use_favicon("/home/derek/Documents/MobileAppGollem/newfavicon/favicon.ico") # path = "path/to/ico". Can be an online file.
+# golem::remove_favicon() # Uncomment to remove the default favicon
+
+## Add helper functions ----
+golem::use_utils_ui(with_test = TRUE)
+golem::use_utils_server(with_test = TRUE)
+
+# You're now set! ----
+
+# go to dev/02_dev.R
+rstudioapi::navigateToFile("dev/02_dev.R")
